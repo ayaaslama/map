ffa2faa278fb66c6905d7f9d0cbb4b10acf680c7
ayaaslama <ayamhmd089@gmial.com>
2024-04-25 14:40:30 +0200

    map_styles&markers

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:map/widgets/custom_google_map.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const TestGoogleMapsWithFlutter());
 }
 
-class MyApp extends StatelessWidget {
+class TestGoogleMapsWithFlutter extends StatelessWidget {
+  const TestGoogleMapsWithFlutter({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Location Picker',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Location Picker',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: CustomGoogleMap());
   }
 }
-
---
+++ lib/widgets/custom_google_map.dart
@@ -0,0 +1,60 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class CustomGoogleMap extends StatefulWidget {
+  const CustomGoogleMap({super.key});
+
+  @override
+  State<CustomGoogleMap> createState() => _CustomGoogleMapState();
+}
+
+class _CustomGoogleMapState extends State<CustomGoogleMap> {
+  late CameraPosition initialCameraPosition;
+  @override
+  void initState() {
+    initialCameraPosition = const CameraPosition(
+        zoom: 8, target: LatLng(30.790380837996715, 30.991261896595972));
+    initMarkers();
+    super.initState();
+  }
+
+  Set<Marker> markers = {};
+  late GoogleMapController googleMapController;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      GoogleMap(
+          markers: markers,
+          onMapCreated: (controller) {
+            googleMapController = controller;
+            initMapStyle();
+          },
+          /*cameraTargetBounds: CameraTargetBounds(LatLngBounds(
+              southwest: const LatLng(23.958499556545753, 26.458827024193344),
+              northeast: const LatLng(30.26442963963889, 33.2260286684982))),*/
+          initialCameraPosition: initialCameraPosition),
+      Positioned(
+          bottom: 15,
+          left: 100,
+          right: 100,
+          child:
+              ElevatedButton(onPressed: () {}, child: const Text("Book Now")))
+    ]);
+  }
+
+  void initMapStyle() async {
+    var lightMapStyle = await DefaultAssetBundle.of(context)
+        .loadString("assets/map_styles/light_map_style.json");
+    googleMapController.setMapStyle(lightMapStyle);
+  }
+
+  void initMarkers() {
+    var myMarker = const Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(30.790380837996715, 30.991261896595972));
+    markers.add((myMarker));
+  }
+}
